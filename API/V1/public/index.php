c4b4b5650bd9266af329e3fbe7918726e2da85d4
mouayad03 <116551140+mouayad03@users.noreply.github.com>
2023-02-28 08:48:49 +0100

    places list done
    places list done

--- API/V1/public/index.php
+++ API/V1/public/index.php
@@ -1,96 +1,63 @@
 <?php
+	ini_set('display_errors', '1');
+	ini_set('display_startup_errors', '1');
+	error_reporting(E_ALL);
 
-    //error handler function
-    function customError($errno, $errstr) {
-        echo " ";
-    }
-    
-    //set error handler
-    //set_error_handler("customError");
+	use Psr\Http\Message\ResponseInterface as Response;
+	use Psr\Http\Message\ServerRequestInterface as Request;
+	use Slim\Factory\AppFactory;
+	use ReallySimpleJWT\Token;
 
-    // this handel the request and response.
-    use Psr\Http\Message\ResponseInterface as Response; 
-    use Psr\Http\Message\ServerRequestInterface as Request;
+	require __DIR__ . "/../vendor/autoload.php";
 
-    // This allows to Using Slim and build our application.
-    use Slim\Factory\AppFactory;
-    use ReallySimpletoken\Token;
+	require "util/database.php";
 
-    // all the libraries we need.
-    require __DIR__ . "/../vendor/autoload.php";
-    // self made functions
-    require_once "Controler/validation.php";
-    require "Model/users.php";
+	header("Content-Type: application/json");
 
-    // all response data will be in the Json Fromat
-    header("Content-Type: application/json");
+	$app = AppFactory::create();
+	
+	$app->setBasePath("/API/V1");
 
-    $app = AppFactory::create();
+	/**
+	 * @OA\Post(
+	 *     path="/Authenticate",
+	 *     summary="Checks the provided username and password and returns an access token if they are valid. The access token is saved in the cookies.",
+	 *     tags={"Authentication"},
+	 *     requestBody=@OA\RequestBody(
+	 *         request="/Authenticate",
+	 *         required=true,
+	 *         description="Username and password",
+	 *         @OA\MediaType(
+	 *             mediaType="application/json",
+	 *             @OA\Schema(
+	 *                 @OA\Property(property="username", type="string", example="root"),
+	 *                 @OA\Property(property="password", type="string", example="sUP3R53CR3T#")
+	 *             )
+	 *         )
+	 *     ),
+	 *     @OA\Response(response="200", description="Success")
+	 * )
+	 */
+	$app->post("/Authenticate", function (Request $request, Response $response, $args) {
+		$data = json_decode(file_get_contents("php://input"), true);
 
-    $app->setBasePath("/API/V1");
+		require "util/config.php";
+		if (!$data || !isset($data["username"]) || !isset($data["password"]) || $data["username"] != $api_username || $data["password"] != $api_password) {
+			http_response_code(401);
+			die("Invalid credentials.");
+		}
 
-/**
- * This will work
- * @param args 
- * @param request_body 
- * @return response 
- */
-$app->post("/Login", function (Request $request, Response $response, $args) {
+		//Generate the access token and store it in a cookie.
+		$token = Token::create($data["username"], $data["password"], time() + 3600, "localhost");
+		setcookie("token", $token, time() + 3600);
 
-    // reads the requested JSON body
-    $body_content = file_get_contents("php://input");
-    $JSON_data = json_decode($body_content, true);
+		echo "Success.";
 
-    // if JSON data doesn't have these then there is an error
-    if (isset($JSON_data["username"]) && isset($JSON_data["password"])) {
-    } else {
-        error_function(400, "Empty request");
-    }
+		return $response;
+	});
 
-    // Prepares the data to prevent bad data, SQL injection andCross site scripting
-    $name = validate_string($JSON_data["username"]);
-    $password = validate_string($JSON_data["password"]);
+	require "routes/product.php";
+	require "routes/category.php";
 
-        if (!$password) {
-            error_function(400, "password is invalid, must contain at least 5 characters");
-        }
-        if (!$name) {
-            error_function(400, "username is invalid, must contain at least 5 characters");
-        }
-
-    $password = hash("sha256", $password);
-
-    $user = get_user_by_username($name);
-
-    if ($user["password_hash"] !==  $password) {
-        error_function(404, "not Found");
-    }
-
-    $token = create_token($name, $password, $user["id"]);
-
-    setcookie("token", $token);
-
-    message_function(200, "Succesfuly created Token");
-
-    return $response;
-});
-
-function user_validation(){
-    $current_user = validate_token();
-    if ($current_user === false) {
-        error_function(403, "unauthenticated");
-    }
-    return $current_user;
-};
-
-    $app->get("/Users", function (Request $request, Response $response, $args) {
-        //validate_token(); // unotherized pepole will get rejected
-
-        echo json_encode(get_all_users());
-
-        return $response;
-    });
-
-
-    $app->run();
+	$app->run();
 ?>
--- API/V1/public/util/config.php
+++ API/V1/public/util/config.php
@@ -1,4 +1,8 @@
 <?php
+	//API credentials.
+	$api_username = "root";
+	$api_password = "sUP3R53CR3#";
+
 	//Database connection information.
 	$db_hostname = "localhost";
 	$db_username = "root";
