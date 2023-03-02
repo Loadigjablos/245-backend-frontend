dddb2a57b809728630a45e922f5a49c585b89e44
Ngawang Choeden Shankentsang <116551142+NgawangChoedenShankentsang@users.noreply.github.com>
2023-03-02 08:51:13 +0100

    place function changes

--- API/V1/public/Model/place.php
+++ API/V1/public/Model/place.php
@@ -2,10 +2,10 @@
     // Database conection string
     require "util/database.php";
 
-    function get_room($id) {
+    function get_room($place_name) {
         global $database;
 
-        $result = $database->query("SELECT * FROM places where id = $id;");
+        $result = $database->query("SELECT * FROM places where name = '$place_name';");
 
         if ($result == false) {
             error_function(500, "Error");
@@ -33,10 +33,10 @@
 		return true;
     }
 
-    function update_place($id, $position, $name, $type) {
+    function update_place($place_name, $position, $name, $type) {
 		global $database;
 
-		$result = $database->query("UPDATE `places` SET position = '$position', name = '$name', type = '$type' WHERE id = $id;");
+		$result = $database->query("UPDATE `places` SET position = '$position', name = '$name', type = '$type' WHERE name = '$place_name';");
 
 		if (!$result) {
 			return false;
@@ -44,6 +44,27 @@
 		
 		return true;
 	}
+	function get_all_places() {
+        global $database;
+
+        $result = $database->query("SELECT * FROM places;");
+
+        if ($result == false) {
+            error_function(500, "Error");
+        } else if ($result !== true) {
+            if ($result->num_rows > 0) {
+                $result_array = array();
+                while ($places = $result->fetch_assoc()) {
+                    $result_array[] = $places;
+                }
+                return $result_array;
+            } else {
+                error_function(404, "not Found");
+            }
+        } else {
+            error_function(404, "not Found");
+        }
+    }
 
     function delete_place($place_name) {
 		global $database;
